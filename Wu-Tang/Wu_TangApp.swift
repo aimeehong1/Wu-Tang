78584f8c3e853cd6b4c9839861b1d1dc01001b1f
Aimee Hong <honglp@bc.edu>
2024-10-07 17:44:11 -0400

    Initial Commit

---
+++ Wu-Tang/Wu_TangApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_TangApp.swift
+//  Wu-Tang
+//
+//  Created by Aimee Hong on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Wu_TangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wu-TangTests/Wu_TangTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_TangTests.swift
+//  Wu-TangTests
+//
+//  Created by Aimee Hong on 10/7/24.
+//
+
+import Testing
+@testable import Wu_Tang
+
+struct Wu_TangTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
